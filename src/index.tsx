bd45d10f678342d2d5e569fab187c1655efd32b2
Oleh8888 <primaoleg1998@gmail.com>
2021-03-23 15:50:29 +0200

    typescript template update without unnecessary depends

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { QueryClient, QueryClientProvider } from 'react-query'
-import { ReactQueryDevtools } from 'react-query/devtools'
 
 import App from './App'
 import './index.css'
 
-const queryClient = new QueryClient()
-
-ReactDOM.render(
-    <QueryClientProvider client={queryClient}>
-        <App />
-        <ReactQueryDevtools initialIsOpen />
-    </QueryClientProvider>,
-    document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'))
